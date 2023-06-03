d1a7e212984de32693e25f2945e83e5131726b72
Eskil Uhlving Larsen <7443949+picccard@users.noreply.github.com>
2023-06-03 21:31:29 +0200

    day1 part1

---
+++ 2021/python/day1/part1.py
@@ -0,0 +1,40 @@
+data_test: list = [
+    199,
+    200,
+    208,
+    210,
+    200,
+    207,
+    240,
+    269,
+    260,
+    263,
+]
+
+data_real: list = []
+
+
+def count_increases(data: list) -> int:
+    count: int = 0
+
+    if len(data) < 2:
+        return count
+    
+    prevNum: int = data[0]
+    
+    for i, current_num in enumerate(data, start=1):
+        if current_num > prevNum:
+            count += 1
+        prevNum = current_num
+
+    return count
+
+
+with open('./2021/python/day1/numbers.txt', 'r') as numbers:
+    lines = numbers.read().splitlines() # .readlines()
+    data_real = list(map(int, lines))
+
+print(len(data_real))
+
+print(f'Testdata: {count_increases(data_test)}')
+print(f'Realdata: {count_increases(data_real)}')
